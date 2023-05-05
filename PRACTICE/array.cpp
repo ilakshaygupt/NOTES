8edef2701cb6c0b92a9ff8cbc3dadf7541026550
ilakshaygupt <iamlakshay04@gmail.com>
2023-05-05 07:29:36 +0530

    ARRAY STARTED

---
+++ PRACTICE/array.cpp
@@ -0,0 +1,828 @@
+max size =10^6 insider int main 10^7 globally
+
+
+largest element in array
+
+brute-> sort and arr[n-1]
+better-> max=arr[0]  if arr[i]>arr[0]  max =arr[i]
+
+
+
+
+second largest element in array
+
+brute
+
+
+better
+void getElements(int arr[],int n)
+{
+    if(n==0 || n==1)
+        cout<<-1<<" "<<-1<<endl;  // edge case when only one element is present in array
+    int small=INT_MAX,second_small=INT_MAX;
+    int large=INT_MIN,second_large=INT_MIN;
+    int i;
+    for(i=0;i<n;i++)
+    {
+        small=min(small,arr[i]);
+        large=max(large,arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<second_small && arr[i]!=small)
+            second_small=arr[i];
+        if(arr[i]>second_large && arr[i]!=large)
+            second_large=arr[i];
+    }
+
+    cout<<"Second smallest is "<<second_small<<endl;
+    cout<<"Second largest is "<<second_large<<endl;
+}
+
+OPTIMAL
+int secondSmallest(int arr[],int n)
+{
+    if(n<2)
+        return -1;
+    int small = INT_MAX;
+    int second_small = INT_MAX;
+    int i;
+    for(i = 0; i < n; i++) 
+    {
+       if(arr[i] < small)
+       {
+          second_small = small;
+          small = arr[i];
+       }
+       else if(arr[i] < second_small && arr[i] != small)
+       {
+          second_small = arr[i];
+       }
+    }
+   return second_small;     
+}
+int secondLargest(int arr[],int n)
+{
+	if(n<2)
+	return -1;
+    int large=INT_MIN,second_large=INT_MIN;
+    int i;
+    for (i = 0; i < n; i++) 
+    {
+        if (arr[i] > large) 
+        {
+            second_large = large;
+            large = arr[i];
+        }
+ 
+        else if (arr[i] > second_large && arr[i] != large) 
+        {
+            second_large = arr[i];
+        }
+    }
+    return second_large;                
+}
+
+
+CHECK IF ARRAY IS SORTED
+BRUTE
+bool isSorted(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[i])
+        return false;
+    }
+  }
+
+  return true;
+}
+
+
+better
+
+bool isSorted(int arr[], int n) {
+  for (int i = 1; i < n; i++) {
+    if (arr[i] < arr[i - 1])
+      return false;
+  }
+
+  return true;
+}
+
+
+
+REMOVE DUPLICARED IN SORTED ARRAY -> COUNT DUPLICATES
+int removeDuplicates(int arr[], int n) {
+  set < int > set;
+  for (int i = 0; i < n; i++) {
+    set.insert(arr[i]);
+  }
+  int k = set.size();
+  int j = 0;
+  for (int x: set) {
+    arr[j++] = x;
+  }
+  return k;
+}
+
+BETTER - TWO POINTER
+
+int removeDuplicates(int arr[], int n)
+{
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+
+LEFT ROTATE AN ARRAY BY ONE PLACE
+
+BRUTE
+using namespace std;
+void solve(int arr[], int n) {
+  int temp[n];
+  for (int i = 1; i < n; i++) {
+    temp[i - 1] = arr[i];
+  }
+  temp[n - 1] = arr[0];
+  for (int i = 0; i < n; i++) {
+    cout << temp[i] << " ";
+  }
+  cout << endl;
+}
+
+BETTER
+
+void solve(int arr[], int n) {
+  int temp = arr[0]; // storing the first element of array in a variable
+  for (int i = 0; i < n - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[n - 1] = temp; // assigned the value of variable at the last index
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
+
+
+LEFT ROTATE AN ARRAY BY D PLACES
+
+if the number of rotations is a multiple of the size of the array, the rotations will have no effect on the given array.
+BRUTE
+void leftRotate(int arr[], int n, int d) {
+    if (n == 0) return;
+
+    // Get the effective number of rotations:
+    d = d % n;
+
+    //checking if d is a multiple of n:
+    if (d == 0) return;
+
+    int temp[d]; // temporary array
+
+    //step 1: Copying first d elements
+    // in the temporary array:
+    for (int i = 0; i < d; i++) {
+        temp[i] = arr[i];
+    }
+
+    // step 2: Shift last (n-d) elements
+    // to the left by d places in the given array:
+    for (int i = d; i < n; i++) {
+        arr[i - d] = arr[i];
+    }
+
+    //step 3: Place the elements of the temporary
+    // array in the last d places of the given array:
+    for (int i = n - d; i < n; i++) {
+        arr[i] = temp[i - (n - d)];
+    }
+}
+
+OPTIMIZED -> REVERSAL ALGORITHM
+
+
+void leftRotate(int arr[], int n, int d) {
+
+    if (n == 0) return;
+
+    // Get the effective number of rotations:
+    d = d % n;
+
+    //step 1:
+    reverse(arr, arr + d);
+
+    //step 2:
+    reverse(arr + d, arr + n);
+
+    //step 3:
+    reverse(arr, arr + n);
+}
+
+
+MOVE ZEROES TO end
+BRUTE
+void zerosToEnd(int arr[],int n) {
+
+    int temp[n];
+    int k=0;
+    for (int i=0;i<n;i++){
+        if (arr[i]!=0){
+            temp[k]=arr[i];
+            k++;
+        }
+    }
+    
+    while (k<n){
+        temp[k]=0;
+        k++;
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<temp[i]<<" ";
+    }
+}
+OPTIMAL
+
+void zerosToEnd(int arr[], int n) {
+
+  //finding first zero occurrence
+  int k = 0;
+  while (k < n) {
+    if (arr[k] == 0) {
+
+      break;
+    } else {
+      k = k + 1;
+    }
+  }
+
+  //finding zeros and immediate non-zero elements and swapping them
+  int i = k, j = k + 1;
+
+  while (i < n && j < n) {
+    if (arr[j] != 0) {
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i++;
+
+    }
+    j++;
+  }
+  for (i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
+UNION OF TWO ARRAYS
+BRUTE
+ O( (m+n)log(m+n) )
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  map < int, int > freq;
+  vector < int > Union;
+  for (int i = 0; i < n; i++)
+    freq[arr1[i]]++;
+  for (int i = 0; i < m; i++)
+    freq[arr2[i]]++;
+  for (auto & it: freq)
+    Union.push_back(it.first);
+  return Union;
+}
+
+USING set
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  set < int > s;
+  vector < int > Union;
+  for (int i = 0; i < n; i++)
+    s.insert(arr1[i]);
+  for (int i = 0; i < m; i++)
+    s.insert(arr2[i]);
+  for (auto & it: s)
+    Union.push_back(it);
+  return Union;
+}
+
+TWO POINTERS
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  int i = 0, j = 0; // pointers
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+}
+
+INTERSECTION OF TWO ARRAYS
+BRUTE
+vector<int> intersectionOfArrays(vector<int>A, vector<int>B){
+    
+  vector <int> ans;
+  
+  // to maintain visited
+  vector <int> visited(B.size(), 0);  
+  int i = 0, j = 0;
+  
+  for (i = 0; i < A.size(); i++) {
+      
+    for (j = 0; j < B.size(); j++) {
+
+      if (A[i] == B[j] && visited[j] == 0) { 
+      
+        //if element matches and has not been matched with any other before
+        ans.push_back(B[j]);
+        visited[j] = 1;
+
+        break;
+      } 
+      
+      else if (B[j] > A[i]) break; 
+      //because array is sorted , element will not be beyond this
+    }
+  }
+  
+  return ans;
+    
+}
+
+TWO POINTER APPROACH
+
+ vector<int> intersectionOfArrays(vector<int>A, vector<int>B){
+    
+  // Declare ans array.
+  vector <int> ans;
+
+  int i = 0, j = 0; 
+
+  // to traverse the arrays
+  while (i < A.size() && j < B.size()) {
+      
+    //if current element in i is smaller
+    if (A[i] < B[j]) { 
+      i++;
+    } else if (B[j] < A[i]) {
+      j++;
+    } else {
+        
+      //both elements are equal
+      ans.push_back(A[i]); 
+      i++;
+      j++;
+    }
+  }
+  
+  return ans;
+    
+}
+
+
+FIND MISSING NUMBER IN ARRAY
+BRUTE
+int missingNumber(vector<int>&a, int N) {
+
+    // Outer loop that runs from 1 to N:
+    for (int i = 1; i <= N; i++) {
+
+        // flag variable to check
+        //if an element exists
+        int flag = 0;
+
+        //Search the element using linear search:
+        for (int j = 0; j < N - 1; j++) {
+            if (a[j] == i) {
+
+                // i is present in the array:
+                flag = 1;
+                break;
+            }
+        }
+
+        // check if the element is missing
+        //i.e flag == 0:
+
+        if (flag == 0) return i;
+    }
+
+    // The following line will never execute.
+    // It is just to avoid warnings.
+    return -1;
+}
+
+BETTER -> HASHING
+
+int missingNumber(vector<int>&a, int N) {
+
+    int hash[N + 1] = {0}; //hash array
+
+    // storing the frequencies:
+    for (int i = 0; i < N - 1; i++)
+        hash[a[i]]++;
+
+    //checking the freqencies for numbers 1 to N:
+    for (int i = 1; i <= N; i++) {
+        if (hash[i] == 0) {
+            return i;
+        }
+    }
+
+    // The following line will never execute.
+    // It is just to avoid warnings.
+    return -1;
+}
+OPTIMAL
+int missingNumber(vector<int>&a, int N) {
+
+    //Summation of first N numbers:
+    int sum = (N * (N + 1)) / 2;
+
+    //Summation of all array elements:
+    int s2 = 0;
+    for (int i = 0; i < N - 1; i++) {
+        s2 += a[i];
+    }
+
+    int missingNum = sum - s2;
+    return missingNum;
+}
+
+
+int missingNumber(vector<int>&a, int N) {
+
+    int xor1 = 0, xor2 = 0;
+
+    for (int i = 0; i < N - 1; i++) {
+        xor2 = xor2 ^ a[i]; // XOR of array elements
+        xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
+    }
+    xor1 = xor1 ^ N; //XOR up to [1...N]
+
+    return (xor1 ^ xor2); // the missing number
+}
+
+
+MAX CONSECUTIVE ONES
+
+int findMaxConsecutiveOnes(vector < int > & nums) {
+      int cnt = 0;
+      int maxi = 0;
+      for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 1) {
+          cnt++;
+        } else {
+          cnt = 0;
+        }
+
+        maxi = max(maxi, cnt);
+      }
+      return maxi;
+    }
+
+
+Find the number that appears once, and the other numbers twice
+
+BRUTE
+
+int getSingleElement(vector<int> &arr) {
+    // Size of the array:
+    int n = arr.size();
+
+    //Run a loop for selecting elements:
+    for (int i = 0; i < n; i++) {
+        int num = arr[i]; // selected element
+        int cnt = 0;
+
+        //find the occurrence using linear search:
+        for (int j = 0; j < n; j++) {
+            if (arr[j] == num)
+                cnt++;
+        }
+
+        // if the occurrence is 1 return ans:
+        if (cnt == 1) return num;
+    }
+
+    //This line will never execute
+    //if the array contains a single element.
+    return -1;
+}
+
+BETTER
+
+ARRAY HASHING  to first find the maximum element(say maxElement) to get the range of the hash array
+int getSingleElement(vector<int> &arr) {
+
+    //size of the array:
+    int n = arr.size();
+
+    // Find the maximum element:
+    int maxi = arr[0];
+    for (int i = 0; i < n; i++) {
+        maxi = max(maxi, arr[i]);
+    }
+
+    // Declare hash array of size maxi+1
+    // And hash the given array:
+    vector<int> hash(maxi + 1, 0);
+    for (int i = 0; i < n; i++) {
+        hash[arr[i]]++;
+    }
+
+    //Find the single element and return the answer:
+    for (int i = 0; i < n; i++) {
+        if (hash[arr[i]] == 1)
+            return arr[i];
+    }
+
+    //This line will never execute
+    //if the array contains a single element.
+    return -1;
+}
+HASHING USING MAP DATA STRUCTURE
+
+
+int getSingleElement(vector<int> &arr) {
+
+    //size of the array:
+    int n = arr.size();
+
+    // Declare the hashmap.
+    // And hash the given array:
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        mpp[arr[i]]++;
+    }
+
+    //Find the single element and return the answer:
+    for (auto it : mpp) {
+        if (it.second == 1)
+            return it.first;
+    }
+
+    //This line will never execute
+    //if the array contains a single element.
+    return -1;
+}
+
+
+USING XOR
+
+int getSingleElement(vector<int> &arr) {
+    //size of the array:
+    int n = arr.size();
+
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}
+
+
+LONGEST SUBARRAY WITH GIVEN Summation
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        for (int j = i; j < n; j++) { // ending index
+            // add all the elements of
+            // subarray = a[i...j]:
+            long long s = 0;
+            for (int K = i; K <= j; K++) {
+                s += a[K];
+            }
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+NAIVE
+
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        long long s = 0; // Sum variable
+        for (int j = i; j < n; j++) { // ending index
+            // add the current element to
+            // the subarray a[i...j-1]:
+            s += a[j];
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+BETTER -> HASHING
+
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        long long rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+TWO POINTERS
+
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int left = 0, right = 0; // 2 pointers
+    long long sum = a[0];
+    int maxLen = 0;
+    while (right < n) {
+        // if sum > k, reduce the subarray from left
+        // until sum becomes less or equal to k:
+        while (left <= right && sum > k) {
+            sum -= a[left];
+            left++;
+        }
+
+        // if sum = k, update the maxLen i.e. answer:
+        if (sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        // Move forward thw right pointer:
+        right++;
+        if (right < n) sum += a[right];
+    }
+
+    return maxLen;
+}
+
+
+LONGEST SUBARRAY WITH GIVEVN SUM (POSITIVE AND NEGATIVE)
+
+BRUTE
+
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        for (int j = i; j < n; j++) { // ending index
+            // add all the elements of
+            // subarray = a[i...j]:
+            int s = 0;
+            for (int K = i; K <= j; K++) {
+                s += a[K];
+            }
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+
+
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        int s = 0;
+        for (int j = i; j < n; j++) { // ending index
+            // add the current element to
+            // the subarray a[i...j-1]:
+            s += a[j];
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+HASHING
+
+
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size(); // size of the array.
+
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        int rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+
+2SUM
+BETTER HASHING
+string twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return "YES";
+        }
+        mpp[num] = i;
+    }
+    return "NO";
+}
+
+OPTIMIZED 2 POINTER
+string twoSum(int n, vector<int> &arr, int target) {
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            return "YES";
+        }
+        else if (sum < target) left++;
+        else right--;
+    }
+    return "NO";
+}
+
+
+SORT ARRAY OF 0 1 AND 2
