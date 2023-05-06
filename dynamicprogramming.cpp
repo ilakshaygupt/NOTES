0f24ea9ea472ea3c90dbad11f84d7641124bee10
ilakshaygupt <iamlakshay04@gmail.com>
2023-05-06 19:53:18 +0530

    dp on strings consisiting on problems based on LCS

--- dynamicprogramming.cpp
+++ dynamicprogramming.cpp
@@ -2120,18 +2120,281 @@ int cutRod(vector<int>& price,int N) {
   DP ON STRINGS
 
 
+longest common subsequence
+
+MEMOIZATION
+
+
+int lcsUtil(string& s1, string& s2, int ind1, int ind2,  vector<vector<int>>& dp){
+
+    if(ind1<0 || ind2<0)
+        return 0;
+        
+    if(dp[ind1][ind2]!=-1)
+        return dp[ind1][ind2];
+    
+    if(s1[ind1] == s2[ind2])
+        return dp[ind1][ind2] = 1 + lcsUtil(s1,s2,ind1-1,ind2-1,dp);
+    
+    else 
+        return dp[ind1][ind2] = 0 + max(lcsUtil(s1,s2,ind1,ind2-1,dp),lcsUtil
+       (s1,s2,ind1-1,ind2,dp));
+   
+}
+
+int main()
+
+{
+  int n=s1.size();
+  int m=s2.size();
+
+  vector<vector<int>> dp(n,vector<int>(m,-1));
+  return lcsUtil(s1,s2,n-1,m-1,dp);
+}
+
+tabulation
+
+
+int lcs(string s1, string s2) {
+    
+    int n=s1.size();
+    int m=s2.size();
+
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;//base case
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;//base case
+    }
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+}
+
+
+space optimization
+int lcs(string s1, string s2) {
+    
+    int n=s1.size();
+    int m=s2.size();
+
+    vector<int> prev(m+1,0), cur(m+1,0);
+    
+    // Base Case is covered as we have initialized the prev and cur to 0.
+    //dp[i]=cur dp[i-1]=prev
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                cur[ind2] = 1 + prev[ind2-1];
+            else
+                cur[ind2] = 0 + max(prev[ind2],cur[ind2-1]);
+        }
+        prev= cur;
+    }
+    
+    return prev[m];
+}
+
+
+print longest common subsequence
+
+
+void lcs(string s1, string s2) {
+
+  int n = s1.size();
+  int m = s2.size();
+
+  vector < vector < int >> dp(n + 1, vector < int > (m + 1, 0));
+  for (int i = 0; i <= n; i++) {
+    dp[i][0] = 0;
+  }
+  for (int i = 0; i <= m; i++) {
+    dp[0][i] = 0;
+  }
+
+  for (int ind1 = 1; ind1 <= n; ind1++) {
+    for (int ind2 = 1; ind2 <= m; ind2++) {
+      if (s1[ind1 - 1] == s2[ind2 - 1])
+        dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+      else
+        dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+    }
+  }
+
+  int len = dp[n][m];
+  int i = n;
+  int j = m;
+
+  int index = len - 1;
+  string str = "";
+  for (int k = 1; k <= len; k++) {
+    str += "$"; // dummy string
+  }
+
+  while (i > 0 && j > 0) {
+    if (s1[i - 1] == s2[j - 1]) {
+      str[index] = s1[i - 1];
+      index--;
+      i--;
+      j--;
+    } else if (s1[i - 1] > s2[j - 1]) {
+      i--;
+    } else j--;
+  }
+  cout << str;
+}
+
+
+longest commom substring
+
+int lcs(string &s1, string &s2){
+    
+    int n = s1.size();
+    int m = s2.size();
+    
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+    int ans = 0;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]){
+                int val = 1 + dp[i-1][j-1];
+                dp[i][j] = val;
+                ans = max(ans,val);
+            }
+            else
+                dp[i][j] = 0;
+        }
+    }
+    
+    return ans;
+    
+}
+
+
+space optimization
+
+int lcs(string &s1, string &s2){
+	//	Write your code here.
+    
+    int n = s1.size();
+    int m = s2.size();
+    
+    vector<int> prev(m+1,0), cur(m+1,0);
+
+    int ans = 0;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]){
+                int val = 1 + prev[j-1];
+                cur[j] = val;
+                ans = max(ans,val);
+            }
+            else
+                cur[j] = 0;
+        }
+        prev=cur;
+    }
+    
+    return ans;
+    
+}
+
+
+longest palindromic subsequence
+just find lcs of string and its reverse
 
 
 
 
+minimum operations to amke a string palindromic
 
 
 
 
+int lcs(string s1, string s2) {
+    
+    int n=s1.size();
+    int m=s2.size();
+
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
+    }
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+}
+
+int longestPalindromeSubsequence(string s){
+    string t = s;
+    reverse(s.begin(),s.end());
+    return lcs(s,t);
+}
+
+int minInsertion(string s){
+    int n = s.size();
+    int k = longestPalindromeSubsequence(s);
+    return n-k;
+}
 
 
+int lcs(string s1, string s2) {
+    
+    int n=s1.size();
+    int m=s2.size();
+
+    vector<int> prev(m+1,0), cur(m+1,0);
+    
+    // Base Case is covered as we have initialized the prev and cur to 0.
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                cur[ind2] = 1 + prev[ind2-1];
+            else
+                cur[ind2] = 0 + max(prev[ind2],cur[ind2-1]);
+        }
+        prev= cur;
+    }
+    
+    return prev[m];
+}
 
 
+int longestPalindromeSubsequence(string s){
+    string t = s;
+    reverse(s.begin(),s.end());
+    return lcs(s,t);
+}
+
+int minInsertion(string s){
+    int n = s.size();
+    int k = longestPalindromeSubsequence(s);
+    return n-k;
+}
 
 
 
