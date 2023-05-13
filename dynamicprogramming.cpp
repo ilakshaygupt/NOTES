f9d9ddf8638b9f5fcedc17232a9d79aa6db01d91
ilakshaygupt <iamlakshay04@gmail.com>
2023-05-13 17:46:24 +0530

    dp on stocks completed

--- dynamicprogramming.cpp
+++ dynamicprogramming.cpp
@@ -2923,6 +2923,504 @@ bool wildcardMatching(string & S1, string & S2) {
 
 
 
+  BUY AND SELL STOCKS  can do any number transection
+
+
+long getAns(long *Arr, int ind, int buy, int n, vector<vector<long>> &dp ){
+
+    if(ind==n) return 0; //base case
+    
+    if(dp[ind][buy]!=-1)
+        return dp[ind][buy];
+        
+    long profit;
+    
+    if(buy==0){// We can buy the stock
+        profit = max(0+getAns(Arr,ind+1,0,n,dp), -Arr[ind] + getAns(Arr,ind+1,1,n,dp));
+    }
+    
+    if(buy==1){// We can sell the stock
+        profit = max(0+getAns(Arr,ind+1,1,n,dp), Arr[ind] + getAns(Arr,ind+1,0,n,dp));
+    }
+    
+    return dp[ind][buy] = profit;
+}
+
+
+long getMaximumProfit(long *Arr, int n)
+{
+    //Write your code here
+    
+    vector<vector<long>> dp(n,vector<long>(2,-1));
+    
+    if(n==0) return 0;
+    long ans = getAns(Arr,0,0,n,dp);
+    return ans;
+}
+
+
+
+
+tabulation
+
+
+long getMaximumProfit(long *Arr, int n)
+{
+    //Write your code here
+    
+    vector<vector<long>> dp(n+1,vector<long>(2,-1));
+    
+    //base condition
+    dp[n][0] = dp[n][1] = 0;
+    
+    long profit;
+    
+    for(int ind= n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            if(buy==0){// We can buy the stock
+                profit = max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+dp[ind+1][1], Arr[ind] + dp[ind+1][0]);
+            }
+            
+            dp[ind][buy]  = profit;
+        }
+    }
+    return dp[0][0];
+}
+
+SPACE optimizaiton
+
+long getMaximumProfit(long *Arr, int n)
+{
+    //Write your code here
+    
+    vector<long> ahead (2,0);
+    vector<long> cur(2,0);
+    
+    //base condition
+    ahead[0] = ahead[1] = 0;
+    
+    long profit;
+    
+    for(int ind= n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            if(buy==0){// We can buy the stock
+                profit = max(0+ahead[0], -Arr[ind] + ahead[1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+ahead[1], Arr[ind] + ahead[0]);
+            }
+            cur[buy]  = profit;
+        }
+        
+        ahead = cur;
+    }
+    return cur[0];
+}
+
+
+can do only two  transection
+
+memoization
+
+int getAns(vector<int>& Arr, int n, int ind, int buy, int cap, vector<vector<vector<int>>>& dp ){
+
+    if(ind==n || cap==0) return 0; //base case
+    
+    if(dp[ind][buy][cap]!=-1)
+        return dp[ind][buy][cap];
+        
+    int profit;
+    
+    if(buy==0){// We can buy the stock
+        profit = max(0+getAns(Arr,n,ind+1,0,cap,dp), 
+                    -Arr[ind] + getAns(Arr,n,ind+1,1,cap,dp));
+    }
+    
+    if(buy==1){// We can sell the stock
+        profit = max(0+getAns(Arr,n,ind+1,1,cap,dp),
+                    Arr[ind] + getAns(Arr,n,ind+1,0,cap-1,dp));
+    }
+    
+    return dp[ind][buy][cap] = profit;
+}
+
+
+int maxProfit(vector<int>& prices, int n)
+{
+    // Creating a 3d - dp of size [n][2][3]
+    vector<vector<vector<int>>> dp(n,
+                                    vector<vector<int>> 
+                                            (2,vector<int>(3,-1)));
+    
+    return getAns(prices,n,0,0,2,dp);
+   
+}
+
+tabulation
+
+int maxProfit(vector<int>& Arr, int n)
+{
+    // Creating a 3d - dp of size [n+1][2][3] initialized to 0
+    vector<vector<vector<int>>> dp(n+1,
+                                    vector<vector<int>> 
+                                            (2,vector<int>(3,0)));
+                                            
+    // As dp array is intialized to 0, we have already covered the base case
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<=1; buy++){
+            for(int cap=1; cap<=2; cap++){
+                
+                if(buy==0){// We can buy the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap], 
+                                -Arr[ind] + dp[ind+1][1][cap]);
+                 }
+    
+                if(buy==1){// We can sell the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap],
+                                Arr[ind] + dp[ind+1][0][cap-1]);
+                }
+            }
+        }
+    }
+    
+    
+    return dp[0][0][2];
+   
+}
+space optimizaiton
+
+int maxProfit(vector<int>& Arr, int n)
+{
+    
+    vector<vector<int>> ahead(2,vector<int> (3,0));
+    
+    vector<vector<int>> cur(2,vector<int> (3,0));
+    
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<=1; buy++){
+            for(int cap=1; cap<=2; cap++){
+                
+                if(buy==0){// We can buy the stock
+                    cur[buy][cap] = max(0+ahead[0][cap], 
+                                -Arr[ind] + ahead[1][cap]);
+                 }
+    
+                if(buy==1){// We can sell the stock
+                    cur[buy][cap] = max(0+ahead[1][cap],
+                                Arr[ind] + ahead[0][cap-1]);
+                }
+            }
+        }
+        ahead = cur;
+    }
+    
+    return ahead[0][2];
+   
+}
+
+AT MOST K TRANSECTIONS
+
+int getAns(vector<int>& Arr, int n, int ind, int buy, int cap, vector<vector<vector<int>>>& dp ){
+
+    if(ind==n || cap==0) return 0; //base case
+    
+    if(dp[ind][buy][cap]!=-1)
+        return dp[ind][buy][cap];
+        
+    int profit;
+    
+    if(buy==0){// We can buy the stock
+        profit = max(0+getAns(Arr,n,ind+1,0,cap,dp), 
+                    -Arr[ind] + getAns(Arr,n,ind+1,1,cap,dp));
+    }
+    
+    if(buy==1){// We can sell the stock
+        profit = max(0+getAns(Arr,n,ind+1,1,cap,dp),
+                    Arr[ind] + getAns(Arr,n,ind+1,0,cap-1,dp));
+    }
+    
+    return dp[ind][buy][cap] = profit;
+}
+
+
+int maximumProfit(vector<int>& prices, int n, int k)
+{
+    // Creating a 3d - dp of size [n][2][k+1]
+    vector<vector<vector<int>>> dp(n,
+                                    vector<vector<int>> 
+                                            (2,vector<int>(k+1,-1)));
+    
+    return getAns(prices,n,0,0,k,dp);
+   
+}
+
+
+tabulation
+
+int maximumProfit(vector<int>& Arr, int n, int k)
+{
+    // Creating a 3d - dp of size [n+1][2][k+1] initialized to 0
+    vector<vector<vector<int>>> dp(n+1,
+                                    vector<vector<int>> 
+                                            (2,vector<int>(k+1,0)));
+                                            
+    // As dp array is initialized to 0, we have already covered the base case
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<=1; buy++){
+            for(int cap=1; cap<=k; cap++){
+                
+                if(buy==0){// We can buy the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap], 
+                                -Arr[ind] + dp[ind+1][1][cap]);
+                 }
+    
+                if(buy==1){// We can sell the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap],
+                                Arr[ind] + dp[ind+1][0][cap-1]);
+                }
+            }
+        }
+    }
+    
+    
+    return dp[0][0][k];
+   
+}
+
+space optimizaiton
+
+int maxProfit(vector<int>& Arr, int n, int k)
+{
+    
+    vector<vector<int>> ahead(2,vector<int> (k+1,0));
+    
+    vector<vector<int>> cur(2,vector<int> (k+1,0));
+    
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<=1; buy++){
+            for(int cap=1; cap<=k; cap++){
+                
+                if(buy==0){// We can buy the stock
+                    cur[buy][cap] = max(0+ahead[0][cap], 
+                                -Arr[ind] + ahead[1][cap]);
+                 }
+    
+                if(buy==1){// We can sell the stock
+                    cur[buy][cap] = max(0+ahead[1][cap],
+                                Arr[ind] + ahead[0][cap-1]);
+                }
+            }
+        }
+        ahead = cur;
+    }
+    
+    return ahead[0][k];
+   
+}
+
+
+
+buy and sell stocks with cooldown
+//cannnot but after sell
+
+int getAns(vector<int> Arr, int ind, int buy, int n, vector<vector<int>> &dp ){
+
+    if(ind>=n) return 0; //base case
+    
+    if(dp[ind][buy]!=-1)
+        return dp[ind][buy];
+        
+    int profit;
+    
+    if(buy==0){// We can buy the stock
+        profit = max(0+getAns(Arr,ind+1,0,n,dp), -Arr[ind]+getAns(Arr,ind+1,1,n,dp));
+    }
+    
+    if(buy==1){// We can sell the stock
+        profit = max(0+getAns(Arr,ind+1,1,n,dp), Arr[ind]+getAns(Arr,ind+2,0,n,dp));
+    }
+    
+    return dp[ind][buy] = profit;
+}
+
+
+int stockProfit(vector<int> &Arr)
+{
+    int n = Arr.size();
+    vector<vector<int>> dp(n,vector<int>(2,-1));
+    
+    int ans = getAns(Arr,0,0,n,dp);
+    return ans;
+}
+
+
+tabulation
+
+int stockProfit(vector<int> &Arr)
+{
+    int n = Arr.size();
+    vector<vector<int>> dp(n+2,vector<int>(2,0));
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            int profit;
+            
+            if(buy==0){// We can buy the stock
+                profit = max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+dp[ind+1][1], Arr[ind] + dp[ind+2][0]);
+            }
+            
+            dp[ind][buy] = profit;
+        }
+    }
+    
+    return dp[0][0];
+}
+
+
+space optimizaiton
+
+int stockProfit(vector<int> &Arr)
+{
+    int n = Arr.size();
+    vector<int> cur(2,0);
+    vector<int> front1(2,0);
+    vector<int> front2(2,0);
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            int profit;
+            
+            if(buy==0){// We can buy the stock
+                profit = max(0+front1[0], -Arr[ind] + front1[1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+front1[1], Arr[ind] + front2[0]);
+            }
+            
+            cur[buy] = profit;
+        }
+        
+        front2 = front1;
+        front1 = cur;
+        
+    }
+    
+    return cur[0];
+}
+
+
+buy ans sell stocks with transection fees
+
+MEMEOIZATION
+
+int getAns(vector<int> &Arr,int ind,int buy,int n,int fee,vector<vector<int>> &dp){
+
+    if(ind==n) return 0; //base case
+    
+    if(dp[ind][buy]!=-1)
+        return dp[ind][buy];
+        
+    int profit;
+    
+    if(buy==0){// We can buy the stock
+        profit = max(0+getAns(Arr,ind+1,0,n,fee,dp), -Arr[ind] + getAns(Arr,ind+1,1,n,fee,dp));
+    }
+    
+    if(buy==1){// We can sell the stock
+        profit = max(0+getAns(Arr,ind+1,1,n,fee,dp), Arr[ind] -fee + getAns(Arr,ind+1,0,n,fee,dp));
+    }
+    
+    return dp[ind][buy] = profit;
+}
+
+
+int maximumProfit(int n, int fee, vector<int>& Arr)
+{
+    //Write your code here
+    
+    vector<vector<int>> dp(n,vector<int>(2,-1));
+    
+    if(n==0) return 0;
+    int ans = getAns(Arr,0,0,n,fee,dp);
+    return ans;
+}
+
+tabulation
+
+int maximumProfit(int n, int fee, vector<int>& Arr)
+{
+    //Write your code here
+    if(n==0) return 0;
+    
+    vector<vector<int>> dp(n+1,vector<int>(2,0));
+    
+    // base condition is handled by initializing dp array as 0
+    
+      for(int ind= n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            int profit;
+            
+            if(buy==0){// We can buy the stock
+                profit = max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+dp[ind+1][1], Arr[ind] -fee + dp[ind+1][0]);
+            }
+            
+            dp[ind][buy]  = profit;
+        }
+    }
+    
+    return dp[0][0];
+}
+
+
+spce optimizaiton
+
+int maximumProfit(int n, int fee, vector<int>& Arr)
+{
+    if(n==0) return 0;
+    
+    vector<long> ahead (2,0);
+    vector<long> cur(2,0);
+    
+    //base condition
+    ahead[0] = ahead[1] = 0;
+    
+    long profit;
+    
+    for(int ind= n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            if(buy==0){// We can buy the stock
+                profit = max(0+ahead[0], -Arr[ind] + ahead[1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+ahead[1], Arr[ind] -fee + ahead[0]);
+            }
+            cur[buy]  = profit;
+        }
+        
+        ahead = cur;
+    }
+    return cur[0];
+
+}
 
 
 
